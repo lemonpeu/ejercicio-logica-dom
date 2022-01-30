5e61e0027e53d5d79c1abd44dcfff97c1128aceb
lemonpeu <illanesrpaula@gmail.com>
2022-01-30 12:26:33 -0600

    First commit 🎉

---
+++ Instrucciones.md
@@ -0,0 +1,25 @@
+## Instrucciones
+
+1. Lo primero que tenemos que hacer es mostrar una lista de items en el DOM. Por ahora está vacío.
+   El array se encuentra en utils.js
+   (Si, hay una mezcla de idiomas, después lo arreglo)
+
+2. Si tuvieras que mostrar los items por orden alfabético, cómo lo harías?
+
+3. Hay que agregar otra sección a la página. La actual se va a llamar "Fases de la luna", la nueva "Tipos de velas".
+   La idea es que con un click se muestre y oculte cada sección.
+
+4. Hay que mostrar los items de velas en el DOM como hicimos con las fases de la luna.
+   El plus va a ser que vamos a agregar un input type text que, por el momento, no va a hacer nada.
+   Este input va a estar por fuera de las secciones de velas o lunas.
+
+5. Lo que va a hacer nuestro input es buscar los items que escribimos. Si no hay resultado mostrar un mensaje que diga "No se ha encontrado nada".
+   No tiene que ser un buscador en tiempo real. Escribimos, le damos al botón "Buscar" y recién ahí busca si hay resultados.
+
+6. Vamos a agregar ooootra sección, que se va a llamar "Deseos". Acá vamos a agregar un input, un botón y le vamos a permitir al usuario agregar sus deseos/manifestciones.
+
+## A tener en cuenta:
+
+- Se puede modificar el index.html claramente
+- Se puede modificar los estilos.
+- Voy a prestarle atención a los detalles
---
+++ utils.js
@@ -0,0 +1,101 @@
+const moonPhases = [
+  {
+    id: 1,
+    title: "Luna nueva",
+    img: "https://mui.today/__export/1625571673010/sites/mui/img/2021/07/06/luna-nueva-new-moon-05082020.jpg_659574592.jpg",
+  },
+  {
+    id: 2,
+    title: "Luna nueva visible",
+    img: "https://img.blogs.es/anexom/wp-content/uploads/2021/09/fases-de-la-luna.jpg",
+  },
+  {
+    id: 3,
+    title: "Cuarto creciente",
+    img: "https://definicion.de/wp-content/uploads/2017/02/cuarto-creciente.jpg",
+  },
+  {
+    id: 4,
+    title: "Gibosa creciente",
+    img: "https://live.staticflickr.com/5162/5361102179_338dbf190f_b.jpg",
+  },
+  {
+    id: 5,
+    title: "Luna llena",
+    img: "https://media.vogue.mx/photos/61700a6010e90e1b8077aef1/2:3/w_2560%2Cc_limit/1147254055",
+  },
+  {
+    id: 6,
+    title: "Gibosa menguante",
+    img: "https://www.eluniversohoy.net/wp-content/uploads/2019/07/3883899200374.jpg",
+  },
+  {
+    id: 7,
+    title: "Cuarto menguante",
+    img: "https://calendarios.diariosur.es/img/social/Luna22.png",
+  },
+  {
+    id: 8,
+    title: "Luna balsámica",
+    img: "http://3.bp.blogspot.com/-Slk4hIOP8e0/VQcscO9dyOI/AAAAAAAABE8/I6Tj3isFAgo/s1600/unDiaAntesDeLunaNueva.jpg",
+  },
+];
+
+const candlesTypes = [
+  {
+    id: +new Date(),
+    color: "White",
+    description:
+      "promotes serenity and peace and enhances personal strength and insight.",
+  },
+  {
+    id: +new Date(),
+    color: "Black",
+    description: "psychic protection.",
+  },
+  {
+    id: +new Date(),
+    color: "Green",
+    description: "helps bring your ideas to life and amplifies prosperity.",
+  },
+  {
+    id: +new Date(),
+    color: "Blue",
+    description:
+      "connects with your chakras and any emotional wounds that need healing.",
+  },
+  {
+    id: +new Date(),
+    color: "Yellow",
+    description:
+      "enhances your networking and social skills, bringing in new career opportunities.",
+  },
+  {
+    id: +new Date(),
+    color: "Red",
+    description: "encourages love, sex, and passion.",
+  },
+  {
+    id: +new Date(),
+    color: "Pink",
+    description:
+      "this one’s for ~romance~. Placing a pink candle by your home’s doorway will welcome in love.",
+  },
+  {
+    id: +new Date(),
+    color: "Purple",
+    description: "boosts your spiritual enlightenment and creativity.",
+  },
+  {
+    id: +new Date(),
+    color: "Orange",
+    description:
+      "encourages your ambition and helps you broaden your horizons.",
+  },
+  {
+    id: +new Date(),
+    color: "Brown",
+    description:
+      "helps with all things relating to your resources (like health, energy, possessions, pets, endurance, and courage).",
+  },
+];
